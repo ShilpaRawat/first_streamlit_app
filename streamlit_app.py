0b3af0cab948b226902c32aaec551a3da8d67c2a
Shilpa Rawat <120478745+ShilpaRawat@users.noreply.github.com>
2023-01-03 19:18:03 +0530

    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
-streamlit.title('My first streamlit file')
+streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omlette')
-streamlit.text('Oats')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg') 
+streamlit.text('🥑🍞 Avacado Toast')
